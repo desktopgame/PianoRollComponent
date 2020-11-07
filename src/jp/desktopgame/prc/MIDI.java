33714f37952378c28ecfb08fd00508363d0dd18f
Koya <koya0306@outlook.jp>
2020-11-07 18:21:12 +0900

    微修正

--- src/jp/desktopgame/prc/MIDI.java
+++ src/jp/desktopgame/prc/MIDI.java
@@ -40,61 +40,64 @@ public class MIDI {
      */
     public static final Map<String, Integer> DRUM_RMAP;
 
+    private static Map<Integer, String> _DRUM_MAP;
+    private static Map<String, Integer> _DRUM_RMAP;
+
     static {
-        DRUM_MAP = new HashMap<>();
-        DRUM_RMAP = new HashMap<>();
-        DRUM_MAP.put(35, "Acoustic Bass Drum");
-        DRUM_MAP.put(36, "Bass Drum 1");
-        DRUM_MAP.put(37, "Side Stick");
-        DRUM_MAP.put(38, "Acoustic Snare");
-        DRUM_MAP.put(39, "Hand Clap");
-        DRUM_MAP.put(40, "Electric Snare");
-        DRUM_MAP.put(41, "Low Floor Tom");
-        DRUM_MAP.put(43, "High Floor Tom");
-        DRUM_MAP.put(45, "Low Tom");
-        DRUM_MAP.put(47, "Low Mid Tom");
-        DRUM_MAP.put(48, "High Mid Tom");
-        DRUM_MAP.put(50, "High Tom");
-        DRUM_MAP.put(42, "Closed Hi-Hat");
-        DRUM_MAP.put(44, "Pedal Hi-Hat");
-        DRUM_MAP.put(46, "Open Hi-Hat");
-        DRUM_MAP.put(49, "Crash Cymbal 1");
-        DRUM_MAP.put(51, "Ride Cymbal 1");
-        DRUM_MAP.put(51, "Chinese Cymbal");
-        DRUM_MAP.put(53, "Ride Bell");
-        DRUM_MAP.put(55, "Splash Cymbal");
-        DRUM_MAP.put(54, "Tambourine");
-        DRUM_MAP.put(56, "Cowbell");
-        DRUM_MAP.put(57, "Crash Cymbal 2");
-        DRUM_MAP.put(58, "Vibraslap");
-        DRUM_MAP.put(59, "Ride Cymbal 2");
-        DRUM_MAP.put(60, "High Bongo");
-        DRUM_MAP.put(61, "Low Bongo");
-        DRUM_MAP.put(62, "Mute High Conga");
-        DRUM_MAP.put(63, "Open High Conga");
-        DRUM_MAP.put(64, "Low Conga");
-        DRUM_MAP.put(65, "High Timbale");
-        DRUM_MAP.put(66, "Low Timbale");
-        DRUM_MAP.put(67, "High Agogo");
-        DRUM_MAP.put(68, "Low Agogo");
-        DRUM_MAP.put(69, "Cabase");
-        DRUM_MAP.put(70, "Maracas");
-        DRUM_MAP.put(71, "Short Whistle");
-        DRUM_MAP.put(72, "Long Whistle");
-        DRUM_MAP.put(73, "Short Guiro");
-        DRUM_MAP.put(74, "Long Guiro");
-        DRUM_MAP.put(75, "Claves");
-        DRUM_MAP.put(76, "High Wood Block");
-        DRUM_MAP.put(77, "Low Wood Block");
-        DRUM_MAP.put(78, "Mute Cuica");
-        DRUM_MAP.put(79, "Open Cuica");
-        DRUM_MAP.put(80, "Mute Triangle");
-        DRUM_MAP.put(81, "Open Triangle");
-        for (Integer k : DRUM_MAP.keySet()) {
-            DRUM_RMAP.put(DRUM_MAP.get(k), k);
+        _DRUM_MAP = new HashMap<>();
+        _DRUM_RMAP = new HashMap<>();
+        _DRUM_MAP.put(35, "Acoustic Bass Drum");
+        _DRUM_MAP.put(36, "Bass Drum 1");
+        _DRUM_MAP.put(37, "Side Stick");
+        _DRUM_MAP.put(38, "Acoustic Snare");
+        _DRUM_MAP.put(39, "Hand Clap");
+        _DRUM_MAP.put(40, "Electric Snare");
+        _DRUM_MAP.put(41, "Low Floor Tom");
+        _DRUM_MAP.put(43, "High Floor Tom");
+        _DRUM_MAP.put(45, "Low Tom");
+        _DRUM_MAP.put(47, "Low Mid Tom");
+        _DRUM_MAP.put(48, "High Mid Tom");
+        _DRUM_MAP.put(50, "High Tom");
+        _DRUM_MAP.put(42, "Closed Hi-Hat");
+        _DRUM_MAP.put(44, "Pedal Hi-Hat");
+        _DRUM_MAP.put(46, "Open Hi-Hat");
+        _DRUM_MAP.put(49, "Crash Cymbal 1");
+        _DRUM_MAP.put(51, "Ride Cymbal 1");
+        _DRUM_MAP.put(51, "Chinese Cymbal");
+        _DRUM_MAP.put(53, "Ride Bell");
+        _DRUM_MAP.put(55, "Splash Cymbal");
+        _DRUM_MAP.put(54, "Tambourine");
+        _DRUM_MAP.put(56, "Cowbell");
+        _DRUM_MAP.put(57, "Crash Cymbal 2");
+        _DRUM_MAP.put(58, "Vibraslap");
+        _DRUM_MAP.put(59, "Ride Cymbal 2");
+        _DRUM_MAP.put(60, "High Bongo");
+        _DRUM_MAP.put(61, "Low Bongo");
+        _DRUM_MAP.put(62, "Mute High Conga");
+        _DRUM_MAP.put(63, "Open High Conga");
+        _DRUM_MAP.put(64, "Low Conga");
+        _DRUM_MAP.put(65, "High Timbale");
+        _DRUM_MAP.put(66, "Low Timbale");
+        _DRUM_MAP.put(67, "High Agogo");
+        _DRUM_MAP.put(68, "Low Agogo");
+        _DRUM_MAP.put(69, "Cabase");
+        _DRUM_MAP.put(70, "Maracas");
+        _DRUM_MAP.put(71, "Short Whistle");
+        _DRUM_MAP.put(72, "Long Whistle");
+        _DRUM_MAP.put(73, "Short Guiro");
+        _DRUM_MAP.put(74, "Long Guiro");
+        _DRUM_MAP.put(75, "Claves");
+        _DRUM_MAP.put(76, "High Wood Block");
+        _DRUM_MAP.put(77, "Low Wood Block");
+        _DRUM_MAP.put(78, "Mute Cuica");
+        _DRUM_MAP.put(79, "Open Cuica");
+        _DRUM_MAP.put(80, "Mute Triangle");
+        _DRUM_MAP.put(81, "Open Triangle");
+        for (Integer k : _DRUM_MAP.keySet()) {
+            _DRUM_RMAP.put(_DRUM_MAP.get(k), k);
         }
-        DRUM_MAP = Collections.unmodifiableMap(DRUM_MAP);
-        DRUM_RMAP = Collections.unmodifiableMap(DRUM_RMAP);
+        DRUM_MAP = Collections.unmodifiableMap(_DRUM_MAP);
+        DRUM_RMAP = Collections.unmodifiableMap(_DRUM_RMAP);
     }
 
     public static List<NoteLocation> noteListToNoteLocationList(List<Note> notes, int timebase, int beatWidth) {
